f61d99df2fb773cee83b380529360323de6279fd
Carlos O'Ryan <coryan@google.com>
2026-08-19 22:21:47 +0000

    chore: update librarian circa 2026-08-18 (#470)
    In this version of librarian we can automatically import some packages
    with a `@_spi()` attribute. That is handy to hide implementation details
    within `GoogleCloudWkt` and `GoogleCloudGax`.

--- Sources/GoogleCloudEventarcLoggingV1/MessageBusActivity.swift
+++ Sources/GoogleCloudEventarcLoggingV1/MessageBusActivity.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 import GoogleRpc
 
 /// Logged during the processing of an event published to a message bus.
--- Sources/GoogleCloudEventarcLoggingV1/PipelineActivity.swift
+++ Sources/GoogleCloudEventarcLoggingV1/PipelineActivity.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 import GoogleRpc
 
 /// Logged during the processing of a message in a pipeline.
